3db9d0a5e91f5779ea7126b02c7b4792493a9f5f
Shriya Sreeju <shriya@Shriyas-MacBook-Pro.local>
2026-01-27 15:49:09 -0600

    Add Apollo.io website tracker for lead tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -143,6 +143,25 @@ export default function RootLayout({
             type="application/ld+json"
             dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
           />
+          {/* Apollo.io Website Tracker */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                function initApollo(){
+                  var n=Math.random().toString(36).substring(7),
+                      o=document.createElement("script");
+                  o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n;
+                  o.async=!0;
+                  o.defer=!0;
+                  o.onload=function(){
+                    window.trackingFunctions.onLoad({appId:"69792c016956a7001960df4b"})
+                  };
+                  document.head.appendChild(o)
+                }
+                initApollo();
+              `,
+            }}
+          />
         </head>
         <body className="font-sans">
           <Navbar />
